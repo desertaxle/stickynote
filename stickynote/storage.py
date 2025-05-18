cc233c5f59d71c25e9fcf0b7cb749292d55cda07
Alex Streed <alex.s@prefect.io>
2025-05-17 21:29:04 -0500

    Add file-based storage backend
    This is a simple file-based storage backend that stores memoized results on the
    local filesystem making it more persistent than the memory-based storage.

--- stickynote/storage.py
+++ stickynote/storage.py
@@ -1,3 +1,7 @@
+from __future__ import annotations
+
+import asyncio
+from pathlib import Path
 from typing import Protocol
 
 
@@ -10,37 +14,37 @@ class MemoStorage(Protocol):
         """
         Check if a key exists in the backend.
         """
-        ...
+        ...  # pragma: no cover
 
     async def exists_async(self, key: str) -> bool:
         """
         Check if a key exists in the backend.
         """
-        ...
+        ...  # pragma: no cover
 
-    def get(self, key: str) -> str:
+    def get(self, key: str) -> str | None:
         """
         Get the value of a key from the backend.
         """
-        ...
+        ...  # pragma: no cover
 
-    async def get_async(self, key: str) -> str:
+    async def get_async(self, key: str) -> str | None:
         """
         Get the value of a key from the backend.
         """
-        ...
+        ...  # pragma: no cover
 
     def set(self, key: str, value: str) -> None:
         """
         Set the value of a key in the backend.
         """
-        ...
+        ...  # pragma: no cover
 
     async def set_async(self, key: str, value: str) -> None:
         """
         Set the value of a key in the backend.
         """
-        ...
+        ...  # pragma: no cover
 
 
 class MemoryStorage(MemoStorage):
@@ -63,17 +67,17 @@ class MemoryStorage(MemoStorage):
         """
         return key in self.cache
 
-    def get(self, key: str) -> str:
+    def get(self, key: str) -> str | None:
         """
         Get the value of a key from the cache.
         """
-        return self.cache[key]
+        return self.cache.get(key)
 
-    async def get_async(self, key: str) -> str:
+    async def get_async(self, key: str) -> str | None:
         """
         Get the value of a key from the cache.
         """
-        return self.cache[key]
+        return self.cache.get(key)
 
     def set(self, key: str, value: str) -> None:
         """
@@ -86,6 +90,57 @@ class MemoryStorage(MemoStorage):
         Set the value of a key in the cache.
         """
         self.cache[key] = value
+
+
+class FileStorage(MemoStorage):
+    """
+    Disk-based storage for storing and retrieving memoized results.
+    """
+
+    def __init__(self, path: Path | str):
+        self.path = Path(path)
+
+    def exists(self, key: str) -> bool:
+        """
+        Check if a key exists in the file.
+        """
+        return (self.path / key).exists()
+
+    async def exists_async(self, key: str) -> bool:
+        """
+        Check if a key exists in the file.
+        """
+        return await asyncio.to_thread((self.path / key).exists)
+
+    def get(self, key: str) -> str | None:
+        """
+        Get the value of a key from the file.
+        """
+        try:
+            return (self.path / key).read_text()
+        except FileNotFoundError:
+            return None
+
+    async def get_async(self, key: str) -> str | None:
+        """
+        Get the value of a key from the file.
+        """
+        try:
+            return await asyncio.to_thread((self.path / key).read_text)
+        except FileNotFoundError:
+            return None
+
+    def set(self, key: str, value: str) -> None:
+        """
+        Set the value of a key in the file.
+        """
+        (self.path / key).write_text(value)
+
+    async def set_async(self, key: str, value: str) -> None:
+        """
+        Set the value of a key in the file.
+        """
+        await asyncio.to_thread((self.path / key).write_text, value)
 
 
 DEFAULT_STORAGE: MemoStorage = MemoryStorage()
